8cb80e95a3e8bce7a18b7c16c71673502e0cfc71
Murilo Barros <murilodesouzabarros@gmail.com>
2024-02-02 02:55:18 -0300

    removed 'askd' folder, moved teste.js to the main folder. Changed APIs calls from external apis to our internal api.

--- askdça/teste.js
+++ askdça/teste.js
@@ -20,16 +20,10 @@ $(document).ready(function () {
                 const successCallback = (position) => {
                     let latitude = position.coords.latitude;
                     let longitude = position.coords.longitude;
-
-                    $.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyD7V5yBfCHhUv2ONFsSOQH3g2eL8lmlAwU`, (data, status) => {
-                        let x = data.results[5];
-                        $.get(`http://api.weatherapi.com/v1/current.json?key=74390f9162f24f94b9414250240202&q=${x.formatted_address}&aqi=no`, (data, status) => {
-                            let z = data.current;
-                            let lastupdate = z.last_updated;
-                            let temperature_C = z.feelslike_c
-                            let temperature_F = z.feelslike_f
-                            let summary_text = z.condition.text;
-                            let newTemperature = { date: lastupdate, temperatureC: temperature_C, temperatureF: temperature_F, summary: summary_text, userLocation: data.location.country }
+                    $.get(`http://177.70.248.43:3002/LocalWeather?latitude=${latitude}&longitude=${longitude}`, (data, status) => {
+                        let x = data;
+                        $.get(`http://177.70.248.43:3002/LocalWeatherInfos?country=${x}`, (data, status) => {
+                            let newTemperature = { date: data.last_updated, temperatureC: data.feelslike_c, temperatureF: data.feelslike_f, summary: data.condition.text, userLocation: x }
                             this.userTemperature = newTemperature;
                         });
                     });
